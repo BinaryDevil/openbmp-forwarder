cb1f04ed24d3236442aa10ceb916005c585e93f3
tingrli <tingrli@cisco.com>
2016-06-24 16:15:34 -0700

    Implemented new peer selection bmp destination logic. Adjusted config file as well.
    - Takes dest_peer_group as peer matching bmp collectors to send messages to.
    - Now connects multiple sockets as user can define multiple dest_peer_group.
    - Now listens to openbmp.parsed.peer for peer_name.

--- setup.py
+++ setup.py
@@ -11,5 +11,6 @@ setup(name='openbmp-forwarder',
       data_files=[('etc', ['src/etc/openbmp-forwarder.yml'])],
       package_dir={'': 'src/site-packages'},
       packages=['openbmp', 'openbmp.parsed'],
-      scripts=['src/bin/openbmp-forwarder']
-     )
+      scripts=['src/bin/openbmp-forwarder'],
+      install_requires=['ipaddress~=1.0.16', 'PyYAML~=3.11', 'python-snappy~=0.5', 'kafka-python~=1.2.2']
+      )
